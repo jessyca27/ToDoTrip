f92418cf23cb39630f07927afe9674f92d566a3c
rem42 <me@rem42.ninja>
2014-12-03 18:18:49 +0100

    Fix merge ...

--- app/information/information.js
+++ app/information/information.js
@@ -37,31 +37,24 @@ var todotripApp = angular.module('todoTrip.information', ['ngRoute'])
 
 
 todotripApp.controller('ToiletriesListCtrl', function($scope) {
-  $scope.elements = [
-    {'name': 'Serviette de bain', 'value':false},
-    {'name': 'Gel douche', 'value':false},
-    {'name': 'Shampoing', 'value':false},
-    {'name': 'Dentifrice', 'value':false},
-    {'name': 'Brosse à dent', 'value':false},
-    {'name': 'Brosse à cheveux', 'value':false},
-    {'name': 'Maquillage', 'value':false},
-    {'name': 'Sèche cheveux', 'value':false},
-    {'name': 'Rasoir', 'value':false},
-    {'name': 'Mousse à raser', 'value':false},
-    {'name': 'Déodorant', 'value':false},
-    {'name': 'Coupe ongle', 'value':false},
-    {'name': 'Pince à épiler', 'value':false},
-    {'name': 'Gants de toilette', 'value':false},
-    {'name': 'Démaquillant', 'value':false},
-    {'name': 'Disque démaquillant', 'value':false},
-    {'name': 'Crème solaire', 'value':false},
-  ];
-$scope.orderProp = 'name';
+    $scope.elements = [
+        {'name': 'Serviette de bain', 'value':false},
+        {'name': 'Gel douche', 'value':false},
+        {'name': 'Shampoing', 'value':false},
+        {'name': 'Dentifrice', 'value':false},
+        {'name': 'Brosse à dent', 'value':false},
+        {'name': 'Brosse à cheveux', 'value':false},
+        {'name': 'Maquillage', 'value':false},
+        {'name': 'Sèche cheveux', 'value':false},
+        {'name': 'Rasoir', 'value':false},
+        {'name': 'Mousse à raser', 'value':false},
+        {'name': 'Déodorant', 'value':false},
+        {'name': 'Coupe ongle', 'value':false},
+        {'name': 'Pince à épiler', 'value':false},
+        {'name': 'Gants de toilette', 'value':false},
+        {'name': 'Démaquillant', 'value':false},
+        {'name': 'Disque démaquillant', 'value':false},
+        {'name': 'Crème solaire', 'value':false},
+    ];
+    $scope.orderProp = 'name';
 });
-=======
-
-        $("#test").click(function(){
-           $("#test_div").show();
-        });
-}]);
->>>>>>> 23d9388fd122accbf63f45638ad9a1dde5d2b1ef
